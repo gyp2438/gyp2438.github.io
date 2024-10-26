2fb0d8c9830ffe6f56320c830ab9e0dd19a1a2f9
johnlee <johnlee@miltenyi.com>
2024-10-26 12:52:07 -0400

    tutorial finished

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_len=30)
+    name = models.CharField(max_length=30)
+
+    # class Meta:
+    #     verbose_name_plural = "Tags"
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -18,3 +24,19 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
 
     tags = models.ManyToManyField("Tag", related_name="posts")
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # if Post is deleted, also delete the comments
+    # ForeignKeys are many to one - many comments for 1 post
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
+
 
 # Create your views here.
+
+
+def blog_index(request):
+    """
+    Gets all available blog posts to be sent to the view
+
+    Args:
+        request (_type_): _description_
+    """
+    # minus sign -> descending -> most recent first
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
+
+
+def blog_tag(request, tag):
+    """
+    Allow filter by tag
+
+    Args:
+        request (_type_): _description_
+        tag (_type_): _description_
+    """
+    posts = Post.objects.filter(tags__name__contains=tag)
+    posts = posts.order_by('-created_on')
+
+    context = {
+        "category": tag,
+        "posts": posts,
+    }
+
+    return render(request, "blog/tag.html", context)
+
+
+def blog_detail(request, key):
+    """
+    Get a specifc blog entry by primary key (pk)
+
+    Args:
+        request (_type_): _description_
+        key (_type_): _description_
+    """
+    post = Post.objects.get(pk=key)
+
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(post=post)
+
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
+
+    return render(request, "blog/detail.html", context)
