89a15cc369d24ea798cf973a3f379d169517ebaf
johnlee <johnlee@miltenyi.com>
2024-10-27 20:56:30 -0400

    testing static build

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,51 @@
 from django.urls import path
+from django_distill import distill_path
+from .models import Post, Tag
+
 from . import views
+
+
+def get_all_blog_pks():
+    return ((post.pk,) for post in Post.objects.all())
+
+
+def get_all_tags():
+    return ((tag.name,) for tag in Tag.objects.all())
+
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("tag/<tag>/", views.blog_tag, name="blog_tag"),
+]
+
+
+urlpatterns += [
+    # Home page URL
+    distill_path(
+        '',
+        views.blog_index,
+        name='blog_index',
+        distill_file='index.html'  # Home page as index.html
+    ),
+
+    # Detail page URL for blog posts, matches "post/<int:pk>/"
+    distill_path(
+        'post/<int:pk>/',
+        views.blog_detail,
+        name='blog_detail',
+        distill_func=get_all_blog_pks,  # Supplies primary keys for each post
+        # Generates "post/<pk>/index.html"
+        distill_file='post/{0}/index.html'
+    ),
+
+    # Tag page URL, matches "tag/<tag>/"
+    distill_path(
+        'tag/<tag>/',
+        views.blog_tag,
+        name='blog_tag',
+        distill_func=get_all_tags,  # Supplies all unique tags
+        # Generates "tag/<tag>/index.html"
+        distill_file='tag/{0}/index.html'
+    ),
 ]
--- github_io/settings.py
+++ github_io/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_distill",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates/",
+            os.path.join(BASE_DIR, "templates/"),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
